128f5dd5d910d4c99698c41a6f6d448233dc22ca
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-04 21:45:01 -0700

    bug fix

--- tests/testthat/test-fabrication.R
+++ tests/testthat/test-fabrication.R
@@ -39,6 +39,5 @@ fabricate_data(regions = level(N = 5),
 
 region_data <- data.frame(capital = c(1, 0, 0, 0, 0))
 fabricate_data(regions = level(N = 5, gdp = runif(5)),
-               cities = level(N = sample(1:5), subways = rnorm(N, mean = 5)),
-               data = region_data)
+               cities = level(N = sample(1:5), subways = rnorm(N, mean = 5)))
 
