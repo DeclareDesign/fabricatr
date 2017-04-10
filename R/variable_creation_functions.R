4d6c34d34afcc525381a6872205d80227691e5fb
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-10 08:16:09 -0700

    add variable creation functions

---
+++ R/variable_creation_functions.R
@@ -0,0 +1,26 @@
+
+# logistic <- function(x){
+#   1/(exp(-x))
+# }
+
+#' Binary variable from a binomial distribution with a logit link
+#'
+#' @param data
+#'
+#' @export
+binary_logit <- binary_logistic_variable <- function(data){
+  rbinom(n = ifelse(is.vector(data), length(data), nrow(data)),
+         size = 1, prob = 1/(1 + exp(-data)))
+}
+
+#' Count variable from a binomial distribution with a logit link
+#'
+#' @param data
+#'
+#' @param k number of binomial trials, i.e. maximum of the count variable
+#'
+#' @export
+binomial_count <- function(data, k){
+  rbinom(ifelse(is.vector(data), length(data), nrow(data)), size = k, prob = 1 / (1 + exp(-data)))
+}
+
