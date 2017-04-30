862e6e5599ca7b208798518cb047787667f94e38
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-30 10:46:58 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -18,7 +18,7 @@ addToDrat(){
   git fetch upstream 2>err.txt
   git checkout master
 
-  Rscript -e "for(pkg in dir('..', pattern = ifelse(.Platform\$OS.type == 'windows', '.zip', '.t*z'))) { \
+  Rscript -e "for(pkg in dir('..', pattern = ifelse(.Platform\$OS.type == 'windows', '.zip', '.t*z'))) { print(paste('processing', pkg)); \
   drat::insertPackage(file = file.path('..', pkg), \
   repodir = '.', \
   commit='Travis update ${APPVEYOR_PROJECT_NAME:-$PKG_REPO} build ${APPVEYOR_REPO_COMMIT:-$TRAVIS_COMMIT}', branch = 'master') }"
