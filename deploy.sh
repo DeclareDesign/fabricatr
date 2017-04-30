7a6b4a15cf886ef6a8d4407bbbc004b16a33bd60
Graeme Blair <graemeblair@users.noreply.github.com>
2017-04-29 21:06:14 -0700

    drat

---
+++ deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -o errexit -o nounset
+PKG_REPO=$PWD
+cd ..
+
+addToDrat(){
+  mkdir drat; cd drat
+
+  ## Set up Repo parameters
+  git init
+  git config user.name "DeclareDesign Travis"
+  git config user.email "team@declaredesign.org"
+  git config --global push.default simple
+
+  ## Get drat repo
+  git remote add upstream "https://$GH_TOKEN@github.com/DeclareDesign/declaredesign.github.io.git"
+  git fetch upstream 2>err.txt
+  git checkout gh-pages
+
+  Rscript -e "drat::insertPackage(dir("..", pattern = '.tgz'), \
+  repodir = '.', \
+  commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
+
+  Rscript -e "drat::insertPackage(dir("..", pattern = '.tar.gz'), \
+  repodir = '.', \
+  commit='Travis update $PKG_REPO: build $TRAVIS_BUILD_NUMBER')"
+
+  git push 2>err.txt
+
+}
+
+addToDrat
