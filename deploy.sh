24c696cae474b78a3055e6b3432e97689841b4f4
Graeme Blair <graemeblair@users.noreply.github.com>
2017-05-24 16:07:45 -0700

    appveyor

--- deploy.sh
+++ deploy.sh
@@ -23,8 +23,10 @@ Rscript -e "path <- ifelse(.Platform\$OS.type == 'windows', file.path('..', '${A
   for(pkg in dir(path, pattern = ifelse(.Platform\$OS.type == 'windows', '.zip', '.t*z'))) { print(paste('processing', pkg)); \
   drat::insertPackage(file = file.path(path, pkg), \
   repodir = '.', \
-  commit='Travis update ${APPVEYOR_PROJECT_NAME:-$PKG_REPO} build ${APPVEYOR_REPO_COMMIT:-$TRAVIS_COMMIT}', branch = 'master') }"
+  commit = FALSE) }"
 
-dir bin
+dir bin/windows
+
+git commit * -m 'Travis update ${APPVEYOR_PROJECT_NAME:-$PKG_REPO} build ${APPVEYOR_REPO_COMMIT:-$TRAVIS_COMMIT}'
 
 git push 2>err.txt
