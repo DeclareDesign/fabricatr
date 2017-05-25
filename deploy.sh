1b4d04ff802392b58a78fd6e7f3e61b315bcb2f9
Graeme Blair <graemeblair@users.noreply.github.com>
2017-05-24 19:47:29 -0700

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o errexit -o nounset
 PKG_REPO=$PWD
+COMMIT="${TRAVIS_COMMIT:-$APPVEYOR_REPO_COMMIT}"
 cd ..
 
 mkdir drat
@@ -25,6 +26,6 @@ Rscript -e "path <- ifelse(.Platform\$OS.type == 'windows', file.path('..', '${A
 
 git add *
 
-git commit -m "Travis update $PKG_REPO build $TRAVIS_COMMIT"
+git commit -m "Travis update $PKG_REPO build $COMMIT"
 
 git push
