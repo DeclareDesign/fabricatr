fcf012eca9b7c54f0d1662df22181c88855e5ac3
Graeme Blair <graemeblair@users.noreply.github.com>
2017-08-20 13:23:51 -0400

    bug fix tests

--- tests/testthat/test-utilities.R
+++ tests/testthat/test-utilities.R
@@ -5,5 +5,5 @@ test_that("Utilities",{
   expect_equal(format_num(1234.5678), "1234.568")
   expect_equal(format_num(1234.5678, digits=1), "1234.6")
   expect_warning(format_num("Not a valid number"))
-  expect_equal(format_num("Not a valid number"), "NA")
+  suppressWarnings(expect_equal(format_num("Not a valid number"), "NA"))
 })
