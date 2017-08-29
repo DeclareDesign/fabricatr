25d886ed1cc892fa4c966d6b1f3f93dd4abd1de4
Graeme Blair <graemeblair@users.noreply.github.com>
2017-08-28 20:05:53 -0700

    vector -> latent, prob

--- R/variable_creation_functions.R
+++ R/variable_creation_functions.R
@@ -2,7 +2,8 @@
 #'
 #' Drawing binary variables based on probabilities or latent traits is a common task that can be cumbersome.
 #'
-#' @param vector vector representing either the latent variable used to draw the count outcome (if link is "logit" or "probit") or the probability for the count outcome (if link is "identity")
+#' @param latent vector representing the latent variable used to draw the count outcome (if link is "logit" or "probit")
+#' @param prob vector representing the probability for the count outcome (if link is "identity"). Link is automatically set to "identity" if probabilities is provided.
 #' @param link link function between the latent variable and the probability of a postiive outcome, i.e. "logit", "probit", or "identity". For the "identity" link, the latent variable must be a probability.
 #'
 #' @importFrom stats pnorm
@@ -14,28 +15,45 @@
 #' fabricate(N = 10, Y1 = rnorm(N),  Y2 = draw_binary(Y1))
 #' fabricate(N = 10, Y1 = rnorm(N),  Y2 = draw_binary(Y1, link = "logit"))
 #' fabricate(N = 10, Y1 = rnorm(N),  Y2 = draw_binary(Y1, link = "probit"))
-#' fabricate(N = 10, Y1 = runif(N, 0, 1),  Y2 = draw_binary(Y1, link = "identity"))
+#' fabricate(N = 10, Y1 = runif(N, 0, 1),  Y2 = draw_binary(prob = Y1))
 #'
 #' draw_binary(rnorm(10))
 #' draw_binary(rnorm(10), link = "logit")
 #' draw_binary(rnorm(10), link = "probit")
-#' draw_binary(runif(10, 0, 1), link = "identity")
+#' draw_binary(prob = runif(10, 0, 1))
+#' draw_binary(prob = runif(10, 0, 1), link = "identity")
 #'
-draw_binary <- function(vector, link = "logit") {
-  if (!link %in% c("logit", "probit", "identity")) {
-    stop("Please choose either 'logit', 'probit', or 'identity' as a link.")
+draw_binary <- function(latent = NULL, prob = NULL, link = "logit") {
+  if (!is.null(prob)) {
+    link <- "identity"
   }
 
-  if (mode(vector) != "numeric") {
-    stop("Please provide a numeric vector to draw_binary.")
+  if (all(!is.null(latent),!is.null(prob))) {
+    stop("Please either provide a vector of numbers to latent or prob, not both.")
+  }
+
+  if (!is.null(latent) & mode(latent) != "numeric") {
+    stop("Please provide a numeric vector for the latent variable to draw_binary.")
+  }
+
+  if (!is.null(prob) & mode(prob) != "numeric") {
+    stop("Please provide a numeric vector to `prob` for draw_binary.")
   }
 
   if (link == "logit") {
-    prob <- 1 / (1 + exp(-vector))
+    if (is.null(latent)) {
+      stop("Please provide a vector of the latent response when you choose the logit link.")
+    }
+    prob <- 1 / (1 + exp(-latent))
   } else if (link == "probit") {
-    prob <- pnorm(vector)
+    if (is.null(latent)) {
+      stop("Please provide a vector of the latent response when you choose the logit link.")
+    }
+    prob <- pnorm(latent)
   } else if (link == "identity") {
-    prob <- vector
+    if (is.null(prob)) {
+      stop("Please provide a vector of the probability of a `yes` response when you choose the identity link.")
+    }
     if (!all(0 < prob & prob < 1)) {
       stop(
         "You chose the identity link, which means your vector should be probabilities. Some values of your vector are outside of (0, 1)."
@@ -52,7 +70,8 @@ draw_binary <- function(vector, link = "logit") {
 #'
 #' Drawing count variables based on probabilities or latent traits is a common task that can be cumbersome.
 #'
-#' @param vector vector representing either the latent variable used to draw the count outcome (if link is "logit" or "probit") or the probability for the count outcome (if link is "identity")
+#' @param latent vector representing the latent variable used to draw the count outcome (if link is "logit" or "probit")
+#' @param prob vector representing the probability for the count outcome (if link is "identity"). Link is automatically set to "identity" if probabilities is provided.
 #' @param k number of binomial trials, i.e. maximum of the count variable
 #' @param link link function between the latent variable and the probability of a postiive outcome, i.e. "logit", "probit", or "identity". For the "identity" link, the latent variable must be a probability.
 #'
@@ -64,27 +83,48 @@ draw_binary <- function(vector, link = "logit") {
 #'
 #' fabricate(N = 10, Y1 = rnorm(N),  Y2 = draw_count(Y1, k = 4))
 #' fabricate(N = 10, Y1 = rnorm(N),  Y2 = draw_count(Y1, k = 4, link = "probit"))
-#' fabricate(N = 10, Y1 = runif(N, 0, 1),  Y2 = draw_count(Y1, k = 4, link = "identity"))
+#' fabricate(N = 10, Y1 = runif(N, 0, 1),  Y2 = draw_count(prob = Y1, k = 4))
 #'
 #' draw_count(rnorm(10), k = 4)
 #' draw_count(rnorm(10), k = 4, link = "logit")
 #' draw_count(rnorm(10), k = 4, link = "probit")
-#' draw_count(runif(10, 0, 1), k = 4, link = "identity")
-draw_count <- function(vector, k, link = "logit") {
+#' draw_count(prob = runif(10, 0, 1), k = 4)
+#' draw_count(prob = runif(10, 0, 1), k = 4, link = "identity")
+draw_count <- function(latent = NULL, prob = NULL, k, link = "logit") {
+  if (!is.null(prob)) {
+    link <- "identity"
+  }
+
+  if (all(!is.null(latent),!is.null(prob))) {
+    stop("Please either provide a vector of numbers to latent or prob, not both.")
+  }
+
   if (!link %in% c("logit", "probit", "identity")) {
     stop("Please choose either 'logit', 'probit', or 'identity' as a link.")
   }
 
-  if (mode(vector) != "numeric") {
-    stop("Please provide a numeric vector to draw_binary.")
+  if (!is.null(latent) & mode(latent) != "numeric") {
+    stop("Please provide a numeric vector for the latent variable to draw_count")
+  }
+
+  if (!is.null(prob) & mode(prob) != "numeric") {
+    stop("Please provide a numeric vector to `prob` for draw_count.")
   }
 
   if (link == "logit") {
-    prob <- 1 / (1 + exp(-vector))
+    if (is.null(latent)) {
+      stop("Please provide a vector of the latent response when you choose the logit link.")
+    }
+    prob <- 1 / (1 + exp(-latent))
   } else if (link == "probit") {
-    prob <- pnorm(vector)
+    if (is.null(latent)) {
+      stop("Please provide a vector of the latent response when you choose the logit link.")
+    }
+    prob <- pnorm(latent)
   } else if (link == "identity") {
-    prob <- vector
+    if (is.null(prob)) {
+      stop("Please provide a vector of the probability of a `yes` response when you choose the identity link.")
+    }
     if (!all(0 < prob & prob < 1)) {
       stop(
         "You chose the identity link, which means your vector should be probabilities. Some values of your vector are outside of (0, 1)."
