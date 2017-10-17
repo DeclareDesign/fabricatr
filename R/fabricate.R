f248df95be4b0e46081e0edfd643a6b6a614cf56
Aaron Rudkin <junk_signups@rudkin.ca>
2017-10-17 11:27:47 -0700

    Test harness and error handling for variable creation functions.

--- R/fabricate.R
+++ R/fabricate.R
@@ -55,13 +55,16 @@ fabricate <-
            N = NULL,
            ID_label = NULL,
            ...) {
+
     options <- quos(...)
 
+    # Helper expression to check if provided options are R functions
     functions_or_not <-
       sapply(options, function(i) {
         is_lang(get_expr(i))
       })
 
+    # If it's a multi-level dataset, every argument must be a level
     if (length(functions_or_not) > 0) {
       options_fn <-
         sapply(options[functions_or_not], lang_name) ## function names
@@ -71,22 +74,44 @@ fabricate <-
           "Arguments passed to ... must either all be calls to level() or have no calls to level()."
         )
       }
+      # Cache result: all arguments are levels
       all_levels <-
         all(options_fn == "level") &
         length(options_fn) > 0 & all(functions_or_not)
     } else{
+      # Cache result: all arguments are not levels
       all_levels <- FALSE
     }
 
+    # Processing user-provided data frame
     if (!is.null(data) & !any(class(data) == "data.frame")) {
-      stop(
-        "Please provide a data object to the data argument, e.g. a data.frame, tibble, or sf object."
-      )
+      # Not a data frame, but we might be able to make it one
+      if(any(class(data) == "matrix")) {
+        conversion_result = tryCatch({
+          data <- as.data.frame(data)
+        }, error=function(e) {
+          stop(
+            "Provided data object was a matrix and could not be converted to a data.frame. Please provide a data.frame, tibble, or sf object."
+          )
+        })
+      } else {
+        # Not a data frame, need to error out.
+        stop(
+          "Please provide a data object to the data argument, e.g. a data.frame, tibble, or sf object."
+        )
+      }
     }
 
+    # If there's a user-supplied ID level, substitute and convert to a character.
     ID_label <- substitute(ID_label)
     if (!is.null(ID_label)) {
-      ID_label <- as.character(ID_label)
+      if (is.vector(ID_label) & length(ID_label) > 1) {
+        stop(
+          "Provided ID label is a vector of length more than one. Please leave ID_label empty or provide a single character string as ID_label."
+        )
+      } else {
+        ID_label <- as.character(ID_label)
+      }
     }
 
     # check if all the options are level calls
@@ -128,18 +153,49 @@ fabricate_data_single_level <- function(data = NULL,
                                         ID_label = NULL,
                                         ...,
                                         existing_ID = FALSE) {
+
+  # This should have been taken care of by fabricate, but let's ensure at this level as well
   if (sum(!is.null(data),!is.null(N)) != 1) {
     stop("Please supply either a data.frame or N and not both.")
   }
 
+  # If we provided an N, it must be sane
   if (is.null(data)) {
-    if (length(N) > 1) {
+    # No N provided
+    if (is.null(N)) {
+      stop(
+        "You must provide either data or N to each level of data."
+      )
+    }
+    # N is not numeric or else it's a non-integer number.
+    if (!is.numeric(N) | (!is.integer(N) & !is.vector(N))) {
       stop(paste0(
-        "At the top level, ",
-        ID_label,
-        ", you must provide a single number to N."
+        "The provided N must be an integer number. Provided N was of type ",
+        typeof(N)
       ))
     }
+    # If N is a vector of integers, that's bad too.
+    if (length(N) > 1) {
+      # Make sure the error message shows the ID_label if there is one.
+      if (is.null(ID_label)) {
+        stop(
+          "At the top level, you must provide a single number to N"
+        )
+      } else {
+        stop(paste0(
+          "At the top level, ",
+          ID_label,
+          ", you must provide a single number to N."
+        ))
+      }
+    }
+
+    if (N <= 0) {
+      stop(
+        "N must be a positive integer."
+      )
+    }
+
     # make IDs that are nicely padded
     data <-
       data.frame(sprintf(paste0("%0", nchar(N), "d"), 1:N), stringsAsFactors = FALSE)
@@ -148,6 +204,7 @@ fabricate_data_single_level <- function(data = NULL,
     # note if ID_label is NULL that the ID column name is just "ID" -- so safe
     colnames(data) <- ifelse(is.null(ID_label), "ID", ID_label)
   } else {
+    # User is providing data, but we still want to staple an ID column in if necessary.
     N <- nrow(data)
     if (existing_ID == FALSE) {
       data[, ifelse(is.null(ID_label), "ID", ID_label)] <-
--- tests/testthat/test-variables.R
+++ tests/testthat/test-variables.R
@@ -1,38 +1,109 @@
 context("Variable functions")
 
 test_that("Variable functions", {
+  # Single-level data, logit link, inherit or implicit N
   fabricate(my_level = level(
     N = 10,
     Y1 = rnorm(N),
     Y2 = draw_binary(Y1, link = "logit")
   ))
 
+  # Single level, count, inherit or implicit N
   fabricate(my_level = level(
     N = 10,
     Y1 = rnorm(N, 5),
     Y2 = draw_discrete(Y1, type = "count", k = 3)
   ))
 
+  # Draw binary, implicit N
   draw_binary(runif(100))
+  # Draw count, implicit N
   draw_discrete(runif(100), type = "count", k = 4)
 
-
+  # Logit link
   draw_binary(rnorm(5), link = "logit")
+  # Probit link
   draw_binary(rnorm(5), link = "probit")
+  # Identity link
   draw_binary(runif(5, 0, 1), link = "identity")
 
-  draw_discrete(runif(5, 0, 5), type = "count", k = 5)
+  # Count data
   draw_discrete(runif(5, 0, 5), type = "count", k = 5)
   draw_discrete(runif(5, 0, 1), type = "count", k = 5)
 
-  expect_error(draw_binary(data.frame(my_variable = runif(5, 0, 1))))
-  expect_error(draw_count(data.frame(runif(5, 0, 1)), k = 5))
+  # Verify we're not generating exactly the same data every time
+  expect_equal(
+    all(
+      draw_discrete(runif(50, 0, 5), type="count", k = 5),
+      draw_discrete(runif(50, 0, 5), type="count", k = 5)
+    ), FALSE)
 
+  # Binomial data, same k
+  draw_discrete(x=c(0.2, 0.8), k=10, type="binomial")
+  # Binomial data, different k for each obs.
+  draw_discrete(x=c(0.2, 0.8), k=c(10, 100), type="binomial")
+  # Binomial data, no k specified (should default to 1)
+  draw_discrete(x=c(0.2, 0.8), type="binomial")
+
+  # Binary data, invalid probabilities.
+  expect_error(draw_binary(x=-1, N=10)) # Negative
+  expect_error(draw_binary(x=c("invalid", "probability"), N=10)) # Non-numeric
+  expect_error(draw_binary(x=1.2, N=10)) # Positive outside 0-1
+  expect_error(draw_binary(x=c(0.5, 0.5, "invalid mixed"), N=10)) # Mixed non-numeric
+  expect_error(draw_binary()) # No arguments
+  expect_error(draw_binary(N=10)) # Missing probability
+  expect_error(draw_binary(x=c(0.3, 0.4, 0.5), N=10)) # Not a multiple, don't want to recycle
+
+  # Valid binary data
+  draw_binary(x=c(0.5, 0.9), N=10)
+
+
+  # Binomial data, invalid probabilities
+  expect_error(draw_discrete(x=-1, N=10, type="binomial")) # Negative
+  expect_error(draw_discrete(x=c("invalid", "probability"), N=10, type="binomial")) # Non-numeric
+  expect_error(draw_discrete(x=1.2, N=10, type="binomial")) # Positive outside 0-1
+  expect_error(draw_discrete(x=c(0.5, 0.5, "invalid mixed"), N=10, type="binomial")) # Mixed non-numeric
+  expect_error(draw_discrete()) # No arguments
+  expect_error(draw_discrete(N=10, type="binomial")) # Missing probability
+  expect_error(draw_discrete(x=c(0.3, 0.4, 0.5), N=10, type="binomial")) # Not a multiple, don't want to recycle
+
+  # Binomial data, invalid k
+  expect_error(draw_discrete(x=c(0.2, 0.8), k=NA, type="binomial")) # NA
+  expect_error(draw_discrete(x=c(0.2, 0.8), k="invalid", type="binomial")) # Character
+  expect_error(draw_discrete(x=c(0.2, 0.8), k=0.5, type="binomial")) # Non-integer
+  expect_error(draw_discrete(x=c(0.2, 0.8), k=-1, type="binomial")) # Negative integer
+  expect_error(draw_discrete(x=c(0.2, 0.8), k=c(10, 100, 1000), type="binomial")) # Non-multiple
+  expect_error(draw_discrete(x=c(0.2, 0.8), k=c(10, "mixed invalid"), type="binomial")) # Mixed non-integer
+
+  # Valid binomial draw
+  draw_discrete(x=0.5, k=10, N=25, type="binomial")
+
+  # Invalid variable types
+  expect_error(draw_discrete(x=0.5, N=5, type="invalid-type")) # Invalid variable type
+
+  # Invalid link functions
   expect_error(draw_binary(rnorm(5), link = "link-that-doesn't-exist"))
   expect_error(draw_count(rnorm(5), k = 5, link = "link-that-doesn't-exist"))
 
-  # check for error if you send vectors that aren't probabilities to link identity
-  expect_error(draw_binary(prob = runif(5, 2, 3), link = "identity"))
+  # Invalid count draws
+  expect_error(draw_discrete(x=1, N=5, type="count", link="logit")) # Links are not allowed
+  expect_error(draw_discrete(x="invalid", N=5, type="count")) # Invalid lambda
+  expect_error(draw_discrete(x=-1, N=5, type="count")) # Invalid lambda, negative
+  expect_error(draw_discrete(x=c(1, 2, 3, 4, -1), N=5, type="count")) # Mixed lambdas, one negative
+  expect_error(draw_discrete(x=c(1, 2, 3, 4, "invalid"), N=5, type="count")) # Mixed lambdas, one character
+
+  # Valid count draw
+  draw_discrete(x=5, N=25, type="count")
+
+  # Invalid categorical draws
+  expect_error(draw_discrete(x=c(-1, 0, -0.5), N=3, type="categorical")) # Negative probability
+  expect_error(draw_discrete(x="invalid", N=3, type="categorical")) # Non-numeric probability
+  expect_error(draw_discrete(x=c(0.5, 0.75), N=10, type="categorical", link="probit")) # Link functions not accepted
+
+  # Valid categorical draw
+  draw_discrete(x=matrix(rep(c(0.3, 0.3, 0.4), 3), byrow=TRUE, ncol=3, nrow=3), N=3, type="categorical")
+
+  # Convert vector of probabilities to matrix of probabilities
+  expect_warning(draw_discrete(x=c(0.3, 0.3, 0.4), N=3, type="categorical"))
 
 })
-
