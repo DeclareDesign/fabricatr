18dd32d9f2cdd071fbd706a53f5cfeadfc74cd4e
Neal Fultz <nfultz@gmail.com>
2017-10-24 13:14:00 -0700

    Neil's refactor of fabricate and level (#17)
    * Moving some code around
    
    * Use non-... arguments for passing around expressions
    
    * Update other two usages
    
    * chasing nulls down to tighter scope
    
    * roxygenize
    
    * factoring out genID call
    
    * typo

--- R/fabricate.R
+++ R/fabricate.R
@@ -51,43 +51,20 @@
 #'
 #' @export
 fabricate <-
-  function(data = NULL,
-           N = NULL,
-           ID_label = NULL,
+  function(data,
+           N,
+           ID_label,
            ...) {
     options <- quos(...)
 
-    functions_or_not <-
-      sapply(options, function(i) {
-        is_lang(get_expr(i))
-      })
+    all_levels <- check_all_levels(options)
 
-    if (length(functions_or_not) > 0) {
-      options_fn <-
-        sapply(options[functions_or_not], lang_name) ## function names
-      if (any(options_fn == "level") &
-          !all(options_fn == "level")) {
-        stop(
-          "Arguments passed to ... must either all be calls to level() or have no calls to level()."
-        )
-      }
-      all_levels <-
-        all(options_fn == "level") &
-        length(options_fn) > 0 & all(functions_or_not)
-    } else{
-      all_levels <- FALSE
-    }
-
-    if (!is.null(data) & !any(class(data) == "data.frame")) {
+    if (!missing(data) && !"data.frame"  %in% class(data)) {
       stop(
         "Please provide a data object to the data argument, e.g. a data.frame, tibble, or sf object."
       )
     }
 
-    ID_label <- substitute(ID_label)
-    if (!is.null(ID_label)) {
-      ID_label <- as.character(ID_label)
-    }
 
     # check if all the options are level calls
     if (all_levels) {
@@ -95,7 +72,7 @@ fabricate <-
         # Pop the data from the previous level in the current call
         # Do this if there existing data to start with or
         #   and beginning with the second level
-        if (i > 1 | !is.null(data)) {
+        if (i > 1 | !missing(data)) {
           options[[i]] <- lang_modify(options[[i]], data_internal_ = data)
         }
 
@@ -111,12 +88,21 @@ fabricate <-
       return(data)
 
     } else {
+      if(missing(data)) data <- NULL
+      if(missing(N)) N <- NULL
+      if(missing(ID_label)) ID_label <- NULL
+
+      ID_label <- substitute(ID_label)
+      if (!is.null(ID_label)) {
+        ID_label <- as.character(ID_label)
+      }
+
       fabricate_data_single_level(
         data = data,
         N = N,
         ID_label = ID_label,
         existing_ID = !is.null(data) & is.null(ID_label),
-        ... = ...
+        options = options
       )
     }
   }
@@ -127,60 +113,89 @@ fabricate_data_single_level <- function(data = NULL,
                                         N = NULL,
                                         ID_label = NULL,
                                         ...,
-                                        existing_ID = FALSE) {
-  if (sum(!is.null(data),!is.null(N)) != 1) {
+                                        existing_ID = FALSE,
+                                        options=quos(...)) {
+  if (is.null(data) == is.null(N)) {
     stop("Please supply either a data.frame or N and not both.")
   }
 
-  if (is.null(data)) {
-    if (length(N) > 1) {
-      stop(paste0(
+  if (!is.null(N)) {
+    if (length(N) != 1) {
+      stop(
         "At the top level, ",
         ID_label,
         ", you must provide a single number to N."
-      ))
+      )
     }
-    # make IDs that are nicely padded
-    data <-
-      data.frame(sprintf(paste0("%0", nchar(N), "d"), 1:N), stringsAsFactors = FALSE)
-
-    # this creates column names from ID_label
-    # note if ID_label is NULL that the ID column name is just "ID" -- so safe
-    colnames(data) <- ifelse(is.null(ID_label), "ID", ID_label)
-  } else {
+    data <- data.frame()
+    existing_ID <- FALSE
+  } else if(!is.null(data)){
     N <- nrow(data)
-    if (existing_ID == FALSE) {
-      data[, ifelse(is.null(ID_label), "ID", ID_label)] <-
-        sprintf(paste0("%0", nchar(nrow(data)), "d"), 1:nrow(data))
-    }
   }
 
-  args <- quos(...)
-
-  args_names <- names(args)
-
-  if (length(args) > 0) {
-    for (i in 1:length(args)) {
-      # this was changed to move costly data.frame operations inside the loop
-      #   because previously, if you did rnorm(N) in a level
-      #   it literally did a vector of length N, rather than doing
-      #   it for all of the values of the level above it, i.e. if this
-      #   this is the second level and N = 2, it made a vector of length 2
-      #   even though there were 5 units in the higher level so there should
-      #   have been a vector of 5*2 = 10
-      # NB: this is still not super safe; if the expression returns a thing
-      # of length not exactly to N it's just going to repeat it as it does
-      # data.frame(data_list). Usually this shouldn't be a problem but we may
-      # want a warning or error
-      data_list <- as.list(data)
-      data_list$N <- N
-      data_list[[args_names[i]]] <-
-        eval_tidy(args[[i]], data_list)
-      data_list$N <- NULL
-      data <- data.frame(data_list, stringsAsFactors = FALSE)
-    }
+  if (!existing_ID) {
+    if(is.null(ID_label)) ID_label <- "ID"
+    data <- genID(data, ID_label, N)
   }
 
-  rownames(data) <- NULL
+
+  fab(data, options)
+}
+
+# make IDs that are nicely padded
+genID <- function(data, ID, N=nrow(data)){
+  fmt <- paste0("%0", nchar(N), "d")
+  data[1:N, ID] <- sprintf(fmt, 1:N)
+  data
+}
+
+fab <- function(data, args) {
+  N <- nrow(data)
+  for (i in names(args)) {
+    if(i == "") next #Unnamed args are meaningless?
+
+    # this was changed to move costly data.frame operations inside the loop
+    #   because previously, if you did rnorm(N) in a level
+    #   it literally did a vector of length N, rather than doing
+    #   it for all of the values of the level above it, i.e. if this
+    #   this is the second level and N = 2, it made a vector of length 2
+    #   even though there were 5 units in the higher level so there should
+    #   have been a vector of 5*2 = 10
+    # NB: this is still not super safe; if the expression returns a thing
+    # of length not exactly to N it's just going to repeat it as it does
+    # data.frame(data_list). Usually this shouldn't be a problem but we may
+    # want a warning or error
+    data_list <- as.list(data)
+    data_list[[i]] <-
+      eval_tidy(args[[i]], append(data_list, list(N=N)))
+
+    #TODO Factor this out of loop? It's expensive
+    data <- data.frame(data_list, stringsAsFactors = FALSE, row.names=NULL)
+    args[[i]] <- NULL
+  }
+
   return(data)
 }
+
+check_all_levels <- function(options){
+
+  if (length(options) == 0)  return(FALSE)
+
+
+  is_function <- sapply(options, function(i) {
+      is_lang(get_expr(i))
+  })
+
+  is_level <- "level" == sapply(options[is_function], lang_name) ## function names
+
+  if(length(is_level) == 0) return(FALSE)
+
+  if (any(is_level) != all(is_level)) {
+    stop(
+      "Arguments passed to ... must either all be calls to level() or have no calls to level()."
+    )
+  }
+
+  is_level[1] && length(is_level) == length(options)
+}
+
