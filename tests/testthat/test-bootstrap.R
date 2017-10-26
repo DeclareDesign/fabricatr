86abcb5dc339b485fcdd39e3009bca3c676f8bd3
Aaron Rudkin <junk_signups@rudkin.ca>
2017-10-26 14:35:51 -0700

    Added a skipped test for fabricating and resampling extremely large data

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -24,3 +24,19 @@ test_that("Error handling of Bootstrap", {
   resampled_two_levels <- resample_data(two_levels) # Missing N
   expect_error(resample_data(two_levels, c(100, 10), ID_labels = c("Invalid_ID")))
 })
+
+test_that("Extremely high volume data creation.", {
+  skip("Slows build substantially.")
+  deep_dive_data = fabricate(
+    countries = level(N = 100, gdp = rlnorm(N)),
+    states = level(N = 50, population = rlnorm(N)),
+    cities = level(N = 50, holiday = runif(N, 1, 365)),
+    neighborhoods = level(N = 5, stoplights = draw_binary(x=0.5, N)),
+    houses = level(N = 5, population = runif(N, 1, 5)),
+    people = level(N = population, sex = ifelse(draw_binary(x=0.5, N), "M", "F"))
+  )
+
+  test_resample = resample_data(deep_dive_data,
+                                ID_labels=c("countries", "states", "cities"),
+                                N=c(100, 50, 50))
+})
