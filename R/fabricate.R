b6219fddeb914827b5d81280e79223f0803f012f
Aaron Rudkin <junk_signups@rudkin.ca>
2017-11-02 15:31:03 -0700

    Complete documentation of fabricate.R so it makes sense to me.

--- R/fabricate.R
+++ R/fabricate.R
@@ -55,57 +55,76 @@ fabricate <-
            N,
            ID_label,
            ...) {
+
+    # Each level argument passed to fabricate will be stored in options
+    # as an unevaluated language call.
     options <- quos(...)
 
+    # Let's check if we have nothing but level calls.
     all_levels <- check_all_levels(options)
 
+    # We've got data, but it's not a data frame
     if (!missing(data) && !"data.frame"  %in% class(data)) {
+      # It's not at least 2d
       if(is.null(dim(data))) {
           stop(
             "User provided data must be a data frame. Provided data was low dimensional."
           )
       }
+      # We got something that thinks it is data, but it wasn't explicitly given as data
       if(!"data" %in% names(sys.call())) {
         stop(
           "The data argument must be a data object. The argument call, ", deparse(substitute(data)), ", was not a data object (e.g. a data.frame, tibble, sf object, or convertible matrix)."
         )
       }
+      # Let's see if we can make it a data frame
       tryCatch({
         data = as.data.frame(data)
       }, error=function(e) {
+        # We can't make it a data frame -- this should probably never happen,
+        # since it relies on something with a dim attribute not converting to
+        # a data frame.
         stop(
           "User provided data could not convert to a data frame."
         )
       })
     }
 
-
-    # check if all the options are level calls
+    # They were all level calls, so we need to build the level one by one
     if (all_levels) {
       for (i in seq_along(options)) {
-        # Pop the data from the previous level in the current call
-        # Do this if there existing data to start with or
-        #   and beginning with the second level
+        # If we have been passed data or if we are in at least the second level
+        # pass the existing data frame to the variable data_internal_
+        # as an argument to the level generation call.
         if (i > 1 | !missing(data)) {
           options[[i]] <- lang_modify(options[[i]], data_internal_ = data)
         }
 
-        # Also do a sweet switcheroo with the level names
+        # Adds the variable ID_label_ to the quosure at the current level
+        # equal to the value of names(options)[i] -- the "variable" we're assigning
+        # the level to
         options[[i]] <-
           lang_modify(options[[i]], ID_label_ = names(options)[i])
 
-        # update the current data
+        # Execute the current level call in the context of all the data it can see.
         data <- eval_tidy(options[[i]])
-
       }
 
+      # Return the final, assembled data.
       return(data)
 
     } else {
+      # No level calls, this is single-level data
+
+      # No user provided data
       if(missing(data)) data <- NULL
+      # No user provided N
       if(missing(N)) N <- NULL
+      # No user provided ID_label
       if(missing(ID_label)) ID_label <- NULL
 
+      # Building ID label from what the user provided
+      # It's a language symbol -- if so, use the symbol, not the value
       if(is.symbol(substitute(ID_label))) {
         ID_label <- substitute(ID_label)
         if (!is.null(ID_label)) {
@@ -116,7 +135,7 @@ fabricate <-
           # Vector of length n>1, error
           stop("Provided ID_label must be a character vector of length 1 or variable name.")
         } else if(is.vector(ID_label) & is.numeric(ID_label[1])) {
-          # Numeric ID_label
+          # Numeric ID_label -- this is OK but variable names can't be numeric
           warning("Provided ID_label is numeric and will be prefixed with the character \"X\"")
           ID_label <- as.character(ID_label)
         } else if(is.vector(ID_label) & is.character(ID_label[1])) {
@@ -128,6 +147,7 @@ fabricate <-
         }
       }
 
+      # We have our data, N, ID_label, whether there's an ID label, and pass through any other quosure stuff
       fabricate_data_single_level(
         data = data,
         N = N,
@@ -146,12 +166,17 @@ fabricate_data_single_level <- function(data = NULL,
                                         ...,
                                         existing_ID = FALSE,
                                         options=quos(...)) {
+  # The user provided nothing of what we want.
   if (is.null(data) == is.null(N)) {
     stop("Please supply either a data.frame or N and not both.")
   }
 
+  # They provided an N
   if (!is.null(N)) {
+    # The N is not a single integer but we're at the top level
+    # We know we're at the top level because fabricate_data_single_level is only called if we are
     if (length(N) != 1) {
+      # Error message explaining to the user where they messed up
       stop(
         "At the top level, ",
         ifelse(!is.null(ID_label),
@@ -159,15 +184,18 @@ fabricate_data_single_level <- function(data = NULL,
                ""),
         "you must provide a single number to N"
       )
-    } else if(is.numeric(N) & any(!N%%1 == 0)) {
+    } else if(is.numeric(N) & any(!N%%1 == 0 | N<=0)) {
+      # N has to be an integer above 0
       stop(
-        "The provided N must be an integer number. Provided N was of type ",
+        "The provided N must be an integer number greater than 0. Provided N was of type ",
         typeof(N)
       )
     }
 
+    # N is not numeric
     if(!is.numeric(N)) {
       tryCatch({
+        # Let's try to force it to be numeric.
         N = as.numeric(N)
       }, error=function(e) {
         stop(
@@ -176,74 +204,109 @@ fabricate_data_single_level <- function(data = NULL,
       })
     }
 
+    # Set up a data frame that's blank
     data <- data.frame()
+    # There's no existing ID column because we're generating all the data.
     existing_ID <- FALSE
   } else if(!is.null(data)){
+    # The user gave us data, so we have an N, it's the number of rows
     N <- nrow(data)
   }
 
+  # If there's no ID column, we'll generate one now
   if (!existing_ID) {
+    # If they didn't specify an ID label for the ID column, we just give it the name ID
     if(is.null(ID_label)) ID_label <- "ID"
     data <- genID(data, ID_label, N)
   }
 
-
+  # Let's fab the data.
   fab(data, options)
 }
 
-# make IDs that are nicely padded
 genID <- function(data, ID, N=nrow(data)){
+  # Left-Pad ID variable with zeroes
   fmt <- paste0("%0", nchar(N), "d")
+  # Add it to the data frame.
   data[1:N, ID] <- sprintf(fmt, 1:N)
   data
 }
 
 fab <- function(data, args) {
+  # This was explicitly provided above but ends up getting to be implicit because we
+  # created the ID column with nrow(data) N.
   N <- nrow(data)
-  for (i in names(args)) {
-    if(i == "") next #Unnamed args are meaningless?
 
-    # this was changed to move costly data.frame operations inside the loop
-    #   because previously, if you did rnorm(N) in a level
-    #   it literally did a vector of length N, rather than doing
-    #   it for all of the values of the level above it, i.e. if this
-    #   this is the second level and N = 2, it made a vector of length 2
-    #   even though there were 5 units in the higher level so there should
-    #   have been a vector of 5*2 = 10
-    # NB: this is still not super safe; if the expression returns a thing
-    # of length not exactly to N it's just going to repeat it as it does
-    # data.frame(data_list). Usually this shouldn't be a problem but we may
-    # want a warning or error
-    data_list <- as.list(data)
+  # Convert the provisional DF to a list so that we can access it in the environment
+  # of running the formulae for the next variable
+  data_list = as.list(data)
+
+  if(is.null(names(args)) || any(names(args) == "")) {
+    stop("All variables specified at this level should have names.")
+  }
+
+  # Apparently we allow overwriting the data?
+  #if(any(duplicated(names(args)))) {
+  #  stop("All variables specified at this level should be unique.")
+  #}
+
+  print("Begin fab")
+  print(args)
+
+  for (i in names(args)) {
+    # Debug to get a grip on what's going on here
+    print(paste0("Generating data named ", i))
+    print(args[[i]])
+
+    # i is the variable name
+    # args[[i]] is the formula for this variable
+    # data_list contains the current working environment
+
+    # Add a variable called N so that things have access; then evaluate the current
+    # formula, adding it to the environment. Store it in the working data frame
     data_list[[i]] <-
       eval_tidy(args[[i]], append(data_list, list(N=N)))
 
-    #TODO Factor this out of loop? It's expensive
-    data <- data.frame(data_list, stringsAsFactors = FALSE, row.names=NULL)
+    # If we have two arguments with exactly the same name, this will nuke the first
+    # in the list, allowing the next one to be accessible the next time we get to it.
     args[[i]] <- NULL
   }
 
+  data <- data.frame(data_list, stringsAsFactors = FALSE, row.names=NULL)
+
+  # Return to the complete data frame
   return(data)
 }
 
-check_all_levels <- function(options){
 
+check_all_levels <- function(options){
+  print(options)
+
+  # Passing the options quosures
+  # There were no levels at all
   if (length(options) == 0)  return(FALSE)
 
+  # get_expr returns the expression for an item in a quosure
+  # is_lang checks if it's a function
   is_function <- sapply(options, function(i) {
       is_lang(get_expr(i))
   })
 
+  # lang_name gets function name from a quosure
+  # compare this to level to see if it's a level
   is_level <- "level" == sapply(options[is_function], lang_name) ## function names
 
+  # Return false if we have no level calls
   if(length(is_level) == 0) return(FALSE)
 
+  # If some calls are levels and some aren't, we're unhappy
   if (any(is_level) != all(is_level)) {
     stop(
       "Arguments passed to ... must either all be calls to level() or have no calls to level()."
     )
   }
 
+  # Confirm they're all levels
   is_level[1] && length(is_level) == length(options)
 }
 
