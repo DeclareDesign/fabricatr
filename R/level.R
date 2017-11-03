42855e266cba5b3e5c0a1539430f8d4dd72f12d5
Aaron Rudkin <junk_signups@rudkin.ca>
2017-11-03 15:47:24 -0700

    Detailed documentation of existing level function.

--- R/level.R
+++ R/level.R
@@ -9,11 +9,11 @@ level <-
   function(N = NULL,
            ...) {
 
-    # handle data that is sent from higher levels of the hierarchy
-    # this is done internally through data_internal_, which is passed through
-    # the ...; users can send data to any level through data, but is handled
-    # differently
+    # The dots take all the arguments at the current level, as well as
+    # the provisional working data frame, which is injected at a higher level
     dots <- quos(...)
+
+    # If we were passed a working data frame, let's move it into our current level
     if ("data_internal_" %in% names(dots)) {
       data_internal_ <- eval_tidy(dots[["data_internal_"]])
       dots[["data_internal_"]] <- NULL
@@ -21,6 +21,8 @@ level <-
       data_internal_ <- NULL
     }
 
+    # If we were passed an ID for this level (and if not, something went wrong)
+    # pass it through transparently
     if ("ID_label_" %in% names(dots)) {
       ID_label <- eval_tidy(dots[["ID_label_"]])
       dots[["ID_label_"]] <- NULL
@@ -29,45 +31,41 @@ level <-
     }
 
     if (is.null(data_internal_)) {
-
-      ## if data is not provided to fabricate, this part handles the case
-      ##   of the top level, where data must be created for the first time
+      # We're at a top level case with no provided data
       return(fabricate_data_single_level(N=N, ID_label=ID_label, options=dots))
-
     } else {
-      # at the second level, after data is created, or at the top level if data is provided
-      #  to fabricate, there are two case:
+      # We're at a second level case, or a top-level case with provided data. Two things to proceed:
       # 1. ID_label does not yet exist, in which case we create the level defined by ID_label by expanding dataset based on N
       # 2. ID label already exists, in which case we add variables to an existing level
 
-      # if there is no ID variable, expand the dataset based on the commands in N
+      # If we are getting a new ID label, it's a new level.
       if (!ID_label %in% colnames(data_internal_)) {
+        # Evaluate how many N we need
         N <- eval(substitute(N), envir = data_internal_)
 
+        # Expand the working data frame using ID_label
         data_internal_ <-
           expand_data_by_ID(data = data_internal_,
                             ID_label = ID_label,
                             N = N)
 
-        # now that data_internal_ is the right size, pass to "mutate", i.e., simulate data
+        # Having expanded and added the ID, we can fab the rest of the variables
         return(fabricate_data_single_level(data_internal_, NULL, ID_label, options=dots))
 
       } else {
-        # otherwise assume you are adding variables to an existing level
-        # defined by the level ID variable that exists in the data_internal_
+        # The level already exists, we are adding variables to it.
 
-        # get the set of variable names that are unique within the level you are adding vars to
-        #  so the new vars can be a function of existing ones
+        # Subset the working data frame to data that matters by the level we care about
         level_variables <-
           get_unique_variables_by_level(data = data_internal_, ID_label = ID_label)
-
-        # construct a dataset with only those variables at this level
         data <-
           unique(data_internal_[, unique(c(ID_label, level_variables)),
                                 drop = FALSE])
 
+        # Now, fabricate the new variables at this level
         data <- fabricate_data_single_level(data, NULL, ID_label, existing_ID = TRUE, options=dots)
 
+        # Now, merge the new data frame into the old one to recover the variables we previously dropped
         return(merge(
           data_internal_[, colnames(data_internal_)[!(colnames(data_internal_) %in%
                                                         level_variables)], drop = FALSE],
