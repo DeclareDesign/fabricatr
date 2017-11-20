f894d77b0ec0df6491ba88c62f4245af9cc491d4
Aaron Rudkin <junk_signups@rudkin.ca>
2017-11-20 12:56:32 -0800

    Documentation update Nov 20, 2017

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 fabricatr: Imagine your data before you collect it
 ==================================================
--- docs/articles/getting_started.R
+++ docs/articles/getting_started.R
@@ -12,6 +12,39 @@ my_data <-
 my_data
 
 ## ------------------------------------------------------------------------
+fabricate(N = 3, normal_var = rnorm(N))
+
+## ------------------------------------------------------------------------
+fabricate(N = 3, p = c(0, .5, 1), binary = draw_discrete(p))
+
+## ------------------------------------------------------------------------
+fabricate(N = 3, p = c(0, .5, 1), binary = draw_discrete(p, type = "bernoulli"))
+
+## ------------------------------------------------------------------------
+fabricate(N = 3, x = 10*rnorm(N), binary = draw_discrete(x, type = "bernoulli", link = "probit"))
+
+## ------------------------------------------------------------------------
+fabricate(N = 3, p = c(0, .5, 1), binomial = draw_discrete(p, type = "binomial", k = 10))
+
+## ------------------------------------------------------------------------
+fabricate(N = 3, x = 10*rnorm(N), binomial = draw_discrete(x, type = "binomial", k = 10, link = "logit"))
+
+## ------------------------------------------------------------------------
+set.seed(1)
+fabricate(N = 3, x = 5*rnorm(N), ordered = draw_discrete(x, type = "ordered", breaks = c(-Inf, -1, 1, Inf)))
+
+## ------------------------------------------------------------------------
+set.seed(1)
+fabricate(N = 3, x = 5*rnorm(N), ordered = draw_discrete(x, type = "ordered", breaks = c(-Inf, -1, 1, Inf), link = "probit"))
+
+## ------------------------------------------------------------------------
+fabricate(N = 3, x = c(0,5,100), count = draw_discrete(x, type = "count"))
+
+## ------------------------------------------------------------------------
+fabricate(N = 6, p1 = runif(N), p2 = runif(N), p3 = runif(N),
+          cat = draw_discrete(cbind(p1, p2, p3), type = "categorical"))
+
+## ------------------------------------------------------------------------
 baseline_survey <- fabricate(N = 5, Y_pre = rnorm(N))
 
 my_endline <- fabricate(data = baseline_survey, 
@@ -19,8 +52,8 @@ my_endline <- fabricate(data = baseline_survey,
 my_endline
 
 ## ------------------------------------------------------------------------
-bootsrapped_data <- resample_data(baseline_survey, N = 10)
-bootsrapped_data
+bootstrapped_data <- resample_data(baseline_survey, N = 10)
+bootstrapped_data
 
 ## ------------------------------------------------------------------------
 my_data <-
@@ -31,6 +64,13 @@ my_data <-
 
 my_data_2 <- resample_data(my_data, N = c(3, 5), ID_labels = c("cities", "citizens"))
 my_data_2
+
+## ------------------------------------------------------------------------
+fabricate(
+    cities = level(N = 2),
+    citizens = level(
+      N = 1:2, income = rnorm(N), income_mean_city = ave(income, cities))
+  ) 
 
 ## ------------------------------------------------------------------------
 my_data <-
@@ -84,6 +124,4 @@ my_data <-
 data_frame(Y = sample(1:10, 2)) %>%
   fabricate(lower_level = level(N = 3, Y2 = Y + rnorm(N)))
 my_data
-
-
 
